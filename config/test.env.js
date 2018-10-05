f95c71ac6f72e8549457e046b01faa895012cbe9
limubai <983450435@qq.com>
2018-10-05 20:44:57 +0800

    create project

--- config/test.env.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  NODE_ENV: '"testing"',
-  ENV_CONFIG: '"test"',
-  BASE_API: '"https://api-test"'
-}
--- src/store/getters.js
+++
@@ -1,6 +0,0 @@
-const getters = {
-  sidebar: state => state.app.sidebar,
-  language: state => state.app.language,
-  userInfo: state => state.userInfo.userInfo
-}
-export default getters
